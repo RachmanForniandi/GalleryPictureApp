acb6a3b4ca540d61ed6fb70771f160e82a8298ec
RachmanForniandi <rachmanforniandi@gmail.com>
2019-01-17 16:05:00 +0700

    initial commit make a activity and few fragments

---
+++ app/src/main/java/rachmanforniandi/com/gallerypictureapp/Utils/Functions.java
@@ -0,0 +1,16 @@
+package rachmanforniandi.com.gallerypictureapp.Utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import rachmanforniandi.com.gallerypictureapp.R;
+
+public class Functions {
+
+    public static void switchMainFragment(FragmentActivity fragmentActivity, Fragment fragment){
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container,fragment)
+                .commit();
+    }
+}
