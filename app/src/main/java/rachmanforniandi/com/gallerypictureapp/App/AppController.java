3361a2fde40b04566ce7f33f87b63b62ad2f80b2
RachmanForniandi <rachmanforniandi@gmail.com>
2019-01-20 22:58:50 +0700

    add realm database

---
+++ app/src/main/java/rachmanforniandi/com/gallerypictureapp/App/AppController.java
@@ -0,0 +1,19 @@
+package rachmanforniandi.com.gallerypictureapp.App;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AppController extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name("wallpaper.realms")
+                .build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
